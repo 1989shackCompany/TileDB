0f5102edef96bbe162cda7fed928658cdfe07e1c
Luc Rancourt <lucrancourt@gmail.com>
2022-07-05 16:21:43 +0200

    Updates: adding update value APIs.
    This adds an API to set update values for updates. The API is very
    simple and allows to create an update value, using a field name, value
    and size, with the associated API to free it:
    tiledb_update_value_alloc and tiledb_update_value_free.
    
    The value is then set on the query using a new API:
    tiledb_query_add_update_value.
    
    ---
    TYPE: IMPROVEMENT
    DESC: Updates: adding update value APIs.

--- tiledb/sm/c_api/tiledb.h
+++ tiledb/sm/c_api/tiledb.h
@@ -649,6 +649,9 @@ typedef struct tiledb_query_t tiledb_query_t;
 /** A TileDB query condition object. */
 typedef struct tiledb_query_condition_t tiledb_query_condition_t;
 
+/** A TileDB update value object. */
+typedef struct tiledb_update_value_t tiledb_update_value_t;
+
 /** A virtual filesystem object. */
 typedef struct tiledb_vfs_t tiledb_vfs_t;
 
@@ -658,7 +661,7 @@ typedef struct tiledb_vfs_fh_t tiledb_vfs_fh_t;
 /** A fragment info object. */
 typedef struct tiledb_fragment_info_t tiledb_fragment_info_t;
 
-/** An group object. */
+/** A group object. */
 typedef struct tiledb_group_t tiledb_group_t;
 
 /* ********************************* */
